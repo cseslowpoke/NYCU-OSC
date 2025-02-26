d9d08a9df18b9719a4683e455c3024c35035eb78
cseslowpoke <cseslowpoke@gmail.com>
2025-02-26 20:47:11 +0800

    feat: implement mini UART driver for Raspberry Pi 3B+
    - Initialize Mini UART:
      - Enable Mini UART via AUXENB register
      - Disable TX/RX during configuration (AUX_MU_CNTL_REG = 0)
      - Disable interrupts (AUX_MU_IER_REG = 0)
      - Set 8-bit data mode (AUX_MU_LCR_REG = 3)
      - Disable auto flow control (AUX_MU_MCR_REG = 0)
      - Set baud rate to 115200 (AUX_MU_BAUD = 270)
      - Clear FIFO (AUX_MU_IIR_REG = 6)
      - Enable TX/RX after configuration (AUX_MU_CNTL_REG = 3)
    
    - Implement UART read (`uart_recv()`):
      - Wait until data is ready (AUX_MU_LSR_REG bit 0)
      - Read character from AUX_MU_IO_REG
    
    - Implement UART write (`uart_send()`):
      - Wait until transmitter is empty (AUX_MU_LSR_REG bit 5)
      - Write character to AUX_MU_IO_REG

--- include/uart.h
+++ include/uart.h
@@ -1,7 +1,19 @@
-#define PBASE 0x3f000000
-#define GPFSEL1 0x00200004
-#define GPPUD 0x00200094
-#define GPPUDCLK0 0x00200098
+#define GPFSEL1 ((volatile unsigned int *)0x3f200004)
+#define GPPUD ((volatile unsigned int *)0x3f200094)
+#define GPPUDCLK0 ((volatile unsigned int *)0x3f200098)
+
+#define AUX_ENABLES ((volatile unsigned int *)0x3f215004)
+#define AUX_MU_CNTL ((volatile unsigned int *)0x3f215060)
+#define AUX_MU_IER ((volatile unsigned int *)0x3f215044)
+#define AUX_MU_LCR ((volatile unsigned int *)0x3f21504c)
+#define AUX_MU_MCR ((volatile unsigned int *)0x3f215050)
+#define AUX_MU_BAUD ((volatile unsigned int *)0x3f215068)
+#define AUX_MU_IIR ((volatile unsigned int *)0x3f215048)
+#define AUX_MU_LSR ((volatile unsigned int *)0x3f215054)
+#define AUX_MU_IO ((volatile unsigned int *)0x3f215040)
+
+#define UART_CAN_READ() ((*AUX_MU_LSR) & 0x01)
+#define UART_CAN_WRITE() ((*AUX_MU_LSR) & 0x20)
 
 void uart_init();
 
--- src/uart.c
+++ src/uart.c
@@ -3,24 +3,40 @@
 
 void uart_init() {
   // setting gpio 14 and 15 to alternative function 5 (mini uart)
-  volatile int *GPIO_FUNC_SEL = (int *)(PBASE | GPFSEL1);
-  *GPIO_FUNC_SEL = 0x12000;
+  *GPFSEL1 = 0x12000;
 
   // setting gpio Pull-up/down control register
-  volatile int *GPIO_PUD = (int *)(PBASE | GPPUD);
-  volatile int *GPIO_PUD_CLK = (int *)(PBASE | GPPUDCLK0);
-  *GPIO_PUD = 0x1;
+  *GPPUD = 0x1;
   delay_cycles(150);
-  *GPIO_PUD_CLK = 1 << 15;
+  *GPPUDCLK0 = 1 << 15;
   delay_cycles(150);
-  *GPIO_PUD = 0;
-  *GPIO_PUD_CLK = 0;
+  *GPPUD = 0;
+  *GPPUDCLK0 = 0;
+
+  // setting mini uart registers
+  *AUX_ENABLES = 0;
+  *AUX_MU_CNTL = 0;
+  *AUX_MU_IER = 0;
+  *AUX_MU_LCR = 3;
+  *AUX_MU_MCR = 0;
+  *AUX_MU_BAUD = 270;
+  *AUX_MU_IIR = 6;
+  *AUX_MU_CNTL = 3;
+
   return;
 }
 
-void uart_send(char c) {}
+void uart_send(char c) {
+  while (!UART_CAN_WRITE())
+    ;
+  (*AUX_MU_IO) = c;
+}
 
-char uart_recv() { return 'a'; }
+char uart_recv() {
+  while (!UART_CAN_READ())
+    ;
+  return (char)(*AUX_MU_IO);
+}
 
 void uart_send_string(char *str) {
   while (*str) {
