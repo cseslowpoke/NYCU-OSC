45d43e6cdc1d4f874f92123ab39300f115f8f25f
cseslowpoke <cseslowpoke@gmail.com>
2025-03-14 16:21:23 +0800

    feat: add argparse support to send_kernel.py
    - Allow users to specify kernel image path, serial port, and baud rate.
    - Changed default serial port to `/dev/ttyUSB0` and added argparse options.
    - Made script executable by adding a shebang (`#!/usr/bin/python3`).

--- tools/send_kernel.py
+++ tools/send_kernel.py
@@ -1,16 +1,25 @@
+#!/usr/bin/python3
 import struct
 import serial
-port_path = "/dev/ttyUSB4"
-# port_path = "/dev/pts/14"
-baud_rate = 115200
+import argparse
+from pathlib import Path
 
-with open("build/kernel.img", "rb") as f:
+
+parser = argparse.ArgumentParser(description='Send kernel image to bootloader')
+parser.add_argument('kernel', type=str, default="build/kernel.img", help='kernel image path')
+parser.add_argument('--port', type=str, default="/dev/ttyUSB0", help='serial port path')
+parser.add_argument('--baud', type=int, default=115200, help='baud rate')
+
+args = parser.parse_args()
+
+
+with open(args.kernel, "rb") as f:
     kernel_data = f.read()
 
 checksum = sum(kernel_data) & 0xFFFFFFFF
 header = struct.pack('<III', 0x544F4F42, len(kernel_data), checksum)
 
-with serial.Serial(port_path, baud_rate) as ser:
+with serial.Serial(args.port, args.baud) as ser:
     ser.write(header)
     ser.write(kernel_data)
     print("send kernel")
