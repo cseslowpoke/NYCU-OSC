602a46eee1646172d0534c20903f87fcaf2f2925
cseslowpoke <cseslowpoke@gmail.com>
2025-03-19 13:10:27 +0800

    fix: adjust heap start and end address in linker script and allocator
    - Change `_heap_start` from `0x10000000` to `0x210000` in `linker.ld`
    - Adjust `_heap_end` in `simple_alloc_init()` to `0x80000` offset from `_heap_start`
    - Ensures the heap is correctly allocated within the expected memory region

--- kernel/src/simple_alloc.c
+++ kernel/src/simple_alloc.c
@@ -13,6 +13,6 @@ void *simple_alloc(unsigned int size) {
 
 void simple_alloc_init() {
   heap_start = &_heap_start;
-  heap_end = &_heap_start + 0x10000000;
+  heap_end = &_heap_start + 0x80000;
   heap_current = heap_start;
 }
