66d712ed75dc1015a6401075300a6f8d58cf5dc0
cseslowpoke <cseslowpoke@gmail.com>
2025-05-20 16:00:02 +0800

    feat: refine EL0/EL1 data/instruction abort handling

--- kernel/core/exception.c
+++ kernel/core/exception.c
@@ -23,8 +23,10 @@ void print_exception_imformation(trapframe_t *tf) {
 void default_exception_handler() { print_exception_imformation(NULL); }
 
 #define SVC 0b010101
-#define DataAbort 0b100100
-#define InstAbort 0b100000
+#define DataAbort_El0 0b100100
+#define DataAbort_El1 0b100101
+#define InstAbort_El0 0b100000
+#define InstAbort_El1 0b100001
 
 #define TranslationFault 0b0001
 #define AccessFlagFault 0b0010
@@ -50,6 +52,7 @@ static void handle_data_inst_abort(uint64_t esr_el1, trapframe_t *tf) {
 }
 
 void _el1_lower_el_aarch64_sync_handler(trapframe_t *tf) {
+  DISABLE_IRQ();
   task_struct_t *current = get_current();
   current->trapframe = tf;
   uint64_t esr_el1 = READ_SYSREG(ESR_EL1);
@@ -57,16 +60,16 @@ void _el1_lower_el_aarch64_sync_handler(trapframe_t *tf) {
   switch (esr_ec) {
   case SVC: {
     if (syscall_handler(tf) == -1) {
-      debug_printf("Unknown syscall: %d\r\n", tf->gpr[8]);
+      debug_printf("Unknown syscall: %x\r\n", tf->gpr[8]);
       print_exception_imformation(tf);
     }
     break;
   }
-  case DataAbort: {
+  case DataAbort_El0: {
     handle_data_inst_abort(esr_el1, tf);
     break;
   }
-  case InstAbort: {
+  case InstAbort_El0: {
     handle_data_inst_abort(esr_el1, tf);
     break;
   }
@@ -90,7 +93,33 @@ void _el1_lower_el_aarch64_fiq_handler() {}
 void _el1_lower_el_aarch64_serror_handler() {}
 
 void _el1_current_el_aarch64_sync_handler() {
-  print_exception_imformation(NULL);
+  DISABLE_IRQ();
+  task_struct_t *current = get_current();
+  uint64_t esr_el1 = READ_SYSREG(ESR_EL1);
+  uint64_t esr_ec = (esr_el1 >> 26) & 0x3f;
+  trapframe_t *tf = current->trapframe;
+  switch (esr_ec) {
+  case SVC: {
+    if (syscall_handler(tf) == -1) {
+      debug_printf("Unknown syscall: %x\r\n", tf->gpr[8]);
+      print_exception_imformation(tf);
+    }
+    break;
+  }
+  case DataAbort_El1: {
+    handle_data_inst_abort(esr_el1, tf);
+    break;
+  }
+  case InstAbort_El1: {
+    handle_data_inst_abort(esr_el1, tf);
+    break;
+  }
+  default:
+    print_exception_imformation(tf);
+    ENABLE_IRQ();
+    while (1)
+      ;
+  }
 }
 
 void _el1_current_el_aarch64_irq_handler() { irq_handler_entry(); }
